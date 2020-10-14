0cb05249c06f5fd7987175e2cad070a2fb953b31
anonim-123 <72725752+anonim-123@users.noreply.github.com>
2020-10-14 22:04:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ termux-setup-storage
 
 apt update
 apt install -y git zsh
-git clone https://github.com/Cabbagec/termux-ohmyzsh.git "$HOME/termux-ohmyzsh" --depth 1
+git clone https://github.com/anonim-123/termux-ohmyzsh.git "$HOME/termux-ohmyzsh" --depth 1
 
 mv "$HOME/.termux" "$HOME/.termux.bak.$(date +%Y.%m.%d-%H:%M:%S)"
 cp -R "$HOME/termux-ohmyzsh/.termux" "$HOME/.termux"
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1
+git clone git://github.com/anonim-123/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1
 mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
 cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
 sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
